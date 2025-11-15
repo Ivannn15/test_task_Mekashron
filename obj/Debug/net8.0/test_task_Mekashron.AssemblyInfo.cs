634ba42f956c510d5d28821e56caf666111561ef
Иван <you@example.com>
2025-11-15 19:14:47 +0800

    цууу

--- obj/Debug/net8.0/test_task_Mekashron.AssemblyInfo.cs
+++ obj/Debug/net8.0/test_task_Mekashron.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_task_Mekashron")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3229e6278b0d9578907d227d8c85a7f3c2b1e34b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7acbd863a0df5fdbe83486ded35cf1bd8659887f")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_task_Mekashron")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_task_Mekashron")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
